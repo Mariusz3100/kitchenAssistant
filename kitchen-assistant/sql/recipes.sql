4b49af92971c5244a6c0740ca84e32fdc8d4da51
Mariusz3100 <armaniel667@gmail.com>
2015-09-13 03:07:13 +0200

    tyuyu

--- kitchen-assistant/sql/recipes.sql
+++ kitchen-assistant/sql/recipes.sql
@@ -12,21 +12,13 @@ CREATE TABLE recipe_ingredient (
   rec_id int NOT NULL,
   ing_id int NOT NULL,
   amount real NOT NULL,
-  a_type_id int NOT NULL,
+  amount_type varchar(100) NOT NULL,
 
    CONSTRAINT recipe_ing_pk PRIMARY KEY (rec_ing_id)
 
 
 )
 
-CREATE TABLE amount_type (
-   a_type_id int NOT NULL,
-   nazwa varchar(45) NOT NULL,
-
-   CONSTRAINT amount_type_pk PRIMARY KEY (a_type_id)
-
-
-)
 
 
 
@@ -46,15 +38,7 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
-ALTER TABLE recipe_ingredient ADD CONSTRAINT r_ing_a_type_fk
-FOREIGN KEY (a_type_id)
-REFERENCES amount_type (a_type_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
 
 
 
-insert into amount_type (a_type_id,  nazwa)values(1,'ml');
-insert into amount_type (a_type_id,  nazwa)values(2,'mg');
-insert into amount_type (a_type_id,  nazwa)values(3,'szt');
+
