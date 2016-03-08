6e8355833dd3860c44587c1edeadf4957e3bc12b
Mariusz <Mariusz@Mariusz-laptop>
2016-03-08 16:15:32 +0100

    mjjj

--- kitchen-assistant/sql/dbCreation/HealthRelevantIngs.sql
+++ kitchen-assistant/sql/dbCreation/HealthRelevantIngs.sql
@@ -6,14 +6,14 @@
   CONSTRAINT Health_Relevant_Ingredient_pk PRIMARY KEY (hr_id)
 );
 
-CREATE TABLE Basic_Ingredient_Health_Relevance
+CREATE TABLE Basic_Ingredient_Health_Relevance_Amount
 (
   bihr_id serial not null,
   hr_id integer not null,
   bi_id integer not null,
   coeficient real,
   
-  CONSTRAINT Basic_Ingredient_Health_Relevance_pk PRIMARY KEY (bihr_id)
+  CONSTRAINT Basic_Ingredient_Health_Relevance_Amount_pk PRIMARY KEY (bihr_id)
 );
 
 ALTER TABLE Basic_Ingredient_Health_Relevance ADD CONSTRAINT Basic_Ingredient_Health_Relevance_hr
@@ -23,7 +23,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE Basic_Ingredient_Health_Relevance ADD CONSTRAINT Basic_Ingredient_Health_Relevance_bi
+ALTER TABLE Basic_Ingredient_Health_Relevance_Amount ADD CONSTRAINT Basic_Ingredient_Health_Relevance_Amount_bi
 FOREIGN KEY (bi_id)
 REFERENCES Basic_Ingredient (bi_id)
 ON DELETE NO ACTION
