c37b20736a174c6d2c5897cc0a5b627c40b33203
M <armaniel667@gmail.com>
2018-11-30 03:46:33 +0100

    Removing bindex from test

--- kitchen-assistant/src/main/java/mariusz/ambroziak/kassistant/controllers/bootstrap/TestController.java
+++ kitchen-assistant/src/main/java/mariusz/ambroziak/kassistant/controllers/bootstrap/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import mariusz.ambroziak.kassistant.utils.StringHolder;
+
 
 @Controller
 public class TestController {
@@ -20,15 +22,7 @@ public class TestController {
 		
 		return model;
 	}
-	
-	@RequestMapping(value="/bindex")
-	public ModelAndView b_index() {
-		ModelAndView model = new ModelAndView("bootstrap/boot_EntryPage");
 
-		
-		return model;
-	}
-	
 	
 
 }
