3949c2735f4d4dec6f30f9fd177d7ab69ef0cf87
M <armaniel667@gmail.com>
2019-02-16 19:18:21 +0100

    Saving utility queries for selecting records

---
+++ kitchen-assistant/sql/dataRetrieval/innerjoinIngredientsNutrientsAmounts.sql
@@ -0,0 +1,4 @@
+﻿select * from basic_ingredient_nutrient_amount
+ inner join Nutrient on Nutrient.nu_id=basic_ingredient_nutrient_amount.nu_id 
+ inner join basic_ingredient on basic_ingredient.bi_id=basic_ingredient_nutrient_amount.bi_id
+ where basic_ingredient.name='a'
---
+++ kitchen-assistant/sql/dataRetrieval/selectDataSourcesByIngredientNames.sql
@@ -0,0 +1,2 @@
+﻿select * from basic_ingredient
+  inner join basic_ingredient_nutrient_data_source on basic_ingredient_nutrient_data_source.bi_id=basic_ingredient.bi_id
