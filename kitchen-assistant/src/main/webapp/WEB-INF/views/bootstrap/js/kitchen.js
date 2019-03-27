0cbae29205de72f8ea83ee937eb4ec177408d709
M <armaniel667@gmail.com>
2019-03-27 22:24:18 +0100

    V2.1 Disabling "Parse" button for ingredient choosing list.

--- kitchen-assistant/src/main/webapp/WEB-INF/views/bootstrap/js/kitchen.js
+++ kitchen-assistant/src/main/webapp/WEB-INF/views/bootstrap/js/kitchen.js
@@ -1,36 +1,93 @@
 jQuery(document).ready(function( ) {
-    $('input[type=radio]').change(function() {
-        if(this.value=="inny") {
-                for (i = 0; i < this.parentNode.children.length; i++) {
-                    let e=this.parentNode.children[i];
-                    if("input"==e.nodeName.toLowerCase()
-                        &&"text"==e.type.toLowerCase()
-                        &&"inny-url"==e.className){
-                        e.disabled=!this.checked;
-                    }
-                }
+	$('input[type=radio]').change(function() {
+		if(this.value=="inny") {
+			for (i = 0; i < this.parentNode.children.length; i++) {
+				let e=this.parentNode.children[i];
+				if("input"==e.nodeName.toLowerCase()
+						&&"text"==e.type.toLowerCase()
+						&&"inny-url"==e.className){
+					e.disabled=!this.checked;
+				}
+			}
 
-            } else {
-            let thisRadios=$("[name="+this.name+"]");
-            let innyRadio;
-            for(j = 0; j < thisRadios.length; j++){
-                if(thisRadios[j].value=="inny"){
-                    innyRadio=thisRadios[j];
-                }
-            }
-                for (i = 0; i < innyRadio.parentNode.children.length; i++) {
-                    let e=innyRadio.parentNode.children[i];
-                    if("input"==e.nodeName.toLowerCase()
-                        &&"text"==e.type.toLowerCase()
-                        &&e.name.endsWith("_innyUrl")){
-                        e.disabled=!innyRadio.checked;
-                    }
-                }
-            }
+		} else {
+			let thisRadios=$("[name="+this.name+"]");
+			let innyRadio;
+			for(j = 0; j < thisRadios.length; j++){
+				if(thisRadios[j].value=="inny"){
+					innyRadio=thisRadios[j];
+				}
+			}
+			for (i = 0; i < innyRadio.parentNode.children.length; i++) {
+				let e=innyRadio.parentNode.children[i];
+				if("input"==e.nodeName.toLowerCase()
+						&&"text"==e.type.toLowerCase()
+						&&e.name.endsWith("_innyUrl")){
+					e.disabled=!innyRadio.checked;
+				}
+			}
+		}
 
-    });
-    
- /*   
+	});
+
+
+	function updateSubmitButton(){
+		if($("[name='liczbaSkladnikow']")[0]!=undefined
+				&&Number.parseInt($("[name='liczbaSkladnikow']")[0].value)!=undefined){
+			let liczba=Number.parseInt($("[name='liczbaSkladnikow']")[0].value);
+			let allChecked=true;
+			for(let i=1;i<=liczba;i++){
+				if($("[name='skladnik"+i+"_radio']").length>0){
+					//there is such radio button checklist
+					if($("[name='skladnik"+i+"_radio']:checked").length==0){
+						//...but none of the options were checked
+						allChecked=false;
+					}else{
+						//if the checked option was "inny", we should check if text was input
+						if($("#skladnik"+i+"_innyUrl")[0]!=undefined
+								&&!$("#skladnik"+i+"_innyUrl")[0].disabled
+								&&$("#skladnik"+i+"_innyUrl")[0].value==""){
+							allChecked=false;
+						}
+							
+					}
+	
+				}
+			}
+	
+	
+			
+				let submitButtons=$(".recipe-parsed-submit");
+				if(submitButtons.length>0){
+					if(allChecked){
+						submitButtons[0].disabled=false
+					}else{
+						submitButtons[0].disabled=true
+	
+					}
+				}
+				
+				
+				
+	
+		}
+	}
+	updateSubmitButton();
+	let radioList=$("input[type='radio']");
+	for(let i=0;i<=radioList!=undefined&&i<radioList.length;i++){
+		radioList[i].onchange=function(){
+				updateSubmitButton();
+		}
+	}
+	let inneList=$(".inny-url");
+	for(let i=0;i<=inneList!=undefined&&i<inneList.length;i++){
+		inneList[i].onchange=function(){
+				updateSubmitButton();
+		}
+	}
+	
+	
+	/*   
       let list=$(".dynamic-size");
 
     for (i = 0; i < list.length; i++) {
@@ -52,5 +109,5 @@ jQuery(document).ready(function( ) {
         console.log($(list[i]).css("max-height"));
 
     }
-*/
+	 */
 });
