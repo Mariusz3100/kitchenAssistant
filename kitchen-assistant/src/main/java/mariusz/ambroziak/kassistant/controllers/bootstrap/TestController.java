08f05ab6be88e8ef8d8a0f7bc7459086548396dd
M <armaniel667@gmail.com>
2019-05-18 18:34:41 +0200

    New testing methods for controllers

--- kitchen-assistant/src/main/java/mariusz/ambroziak/kassistant/controllers/bootstrap/TestController.java
+++ kitchen-assistant/src/main/java/mariusz/ambroziak/kassistant/controllers/bootstrap/TestController.java
@@ -62,6 +62,39 @@ public class TestController {
 		
 		return null;
 	}
+	
+	@RequestMapping(value="/xml_categorize_test")
+	public ModelAndView xml_categorize_test() throws IOException, GoogleDriveAccessNotAuthorisedException {
+		CategoryHierarchy.initializeCategoriesFromXml();
+		
+		Category singletonRoot = CategoryHierarchy.getSingletonCategoryRoot();
+
+		ModelAndView mav=new ModelAndView("List");
+		mav.addObject("list",singletonRoot==null?"nothing":singletonRoot.getName());
+		return mav;
+	}
+	
+	@RequestMapping(value="/get_xml_category")
+	public ModelAndView get_xml_category() throws IOException, GoogleDriveAccessNotAuthorisedException {
+		Category createCategoriesFromXmlFile = CategoryHierarchy.createCategoriesFromXmlFile();
+		String results=createCategoriesFromXmlFile.toJsonRepresentation().toString();
+		List<String> list=new ArrayList<String>();
+		list.add(results);
+		ModelAndView mav=new ModelAndView("List");
+		mav.addObject("list",list);
+		return mav;
+	}
+	
+	@RequestMapping(value="/get_static_category")
+	public ModelAndView get_static_category() throws IOException, GoogleDriveAccessNotAuthorisedException {
+		Category createCategoriesFromXmlFile = CategoryHierarchy.initializeCategoriesStaticlly();
+		String results=createCategoriesFromXmlFile.toJsonRepresentation().toString();
+		List<String> list=new ArrayList<String>();
+		list.add(results);
+		ModelAndView mav=new ModelAndView("List");
+		mav.addObject("list",list);
+		return mav;
+	}
 
 	@RequestMapping(value="/xml_read_test")
 	public ModelAndView xml_read_test() throws IOException, GoogleDriveAccessNotAuthorisedException {
@@ -102,6 +135,26 @@ public class TestController {
 		return new ModelAndView();
 	}
 	
+	@RequestMapping(value="/categorisation_test_with_static")
+	public ModelAndView categorisation_test_with_static() {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("cucumber:");
+		
+		Map<Produkt, Category> testedCategoriesFor = Categoriser.testStaticCategoriesFor("cucumber", 10);
+		for(Entry<Produkt, Category> e:testedCategoriesFor.entrySet()) {
+			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue());
+		}
+		
+//		list.add("tomato:");
+//		Map<Produkt, Category> testedCategoriesForTomato = Categoriser.testCategoriesFor("tomato", 10);
+//		for(Entry<Produkt, Category> e:testedCategoriesForTomato.entrySet()) {
+//			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue());
+//		}
+//		
+		ModelAndView modelAndView = new ModelAndView("List");
+		modelAndView.addObject("list",list);
+		return modelAndView;
+	}
 	
 	@RequestMapping(value="/categorisation_test")
 	public ModelAndView categorisation_test() {
@@ -110,14 +163,32 @@ public class TestController {
 		
 		Map<Produkt, Category> testedCategoriesFor = Categoriser.testCategoriesFor("cucumber", 10);
 		for(Entry<Produkt, Category> e:testedCategoriesFor.entrySet()) {
-			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue());
+			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue().getName());
 		}
 		
-		list.add("tomato:");
-		Map<Produkt, Category> testedCategoriesForTomato = Categoriser.testCategoriesFor("tomato", 10);
-		for(Entry<Produkt, Category> e:testedCategoriesForTomato.entrySet()) {
-			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue());
-		}
+//		list.add("tomato:");
+//		Map<Produkt, Category> testedCategoriesForTomato = Categoriser.testCategoriesFor("tomato", 10);
+//		for(Entry<Produkt, Category> e:testedCategoriesForTomato.entrySet()) {
+//			list.add(e.getKey().getNazwa()+" ("+e.getKey().getUrl()+")->"+e.getValue());
+//		}
+//		
+		ModelAndView modelAndView = new ModelAndView("List");
+		modelAndView.addObject("list",list);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/categorisation_test_single")
+	public ModelAndView categorisation_test_single() {
+		ArrayList<String> list=new ArrayList<String>();
+
+		int id=83286592;
+		Map<Produkt, Category> testCategoryProduct = Categoriser.testCategoryProduct(id);
+		Produkt product = testCategoryProduct.keySet().iterator().next();
+		list.add(""+id);
+		list.add(""+product.getNazwa());
+		list.add(""+product.getUrl());
+		list.add(testCategoryProduct.get(product).getName());
+		
 		
 		ModelAndView modelAndView = new ModelAndView("List");
 		modelAndView.addObject("list",list);
